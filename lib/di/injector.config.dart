fffdca729094310cbe64ce1c4b6b63082e690224
Marko Filipovic <mrkfilipovic3@gmail.com>
2022-01-26 11:26:55 +0100

    upgrade dependencies

--- lib/di/injector.config.dart
+++ lib/di/injector.config.dart
@@ -7,11 +7,11 @@
 import 'package:dio/dio.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:logger/logger.dart' as _i7;
+import 'package:logger/logger.dart' as _i4;
 
-import '../blocs/articles/articles_bloc.dart' as _i6;
-import '../network/api_client.dart' as _i4;
-import '../network/repositories/articles/articles_repository.dart' as _i5;
+import '../blocs/articles/articles_bloc.dart' as _i7;
+import '../network/api_client.dart' as _i5;
+import '../network/repositories/articles/articles_repository.dart' as _i6;
 import 'modules.dart' as _i8; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
@@ -22,14 +22,14 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
   final networkModule = _$NetworkModule();
   final loggerModule = _$LoggerModule();
   gh.lazySingleton<_i3.Dio>(() => networkModule.dio());
+  gh.singleton<_i4.Logger>(loggerModule.logger());
   gh.factory<String>(() => networkModule.baseUrl(), instanceName: 'baseUrl');
-  gh.factory<_i4.ApiClient>(() => _i4.ApiClient(get<_i3.Dio>(),
+  gh.factory<_i5.ApiClient>(() => _i5.ApiClient(get<_i3.Dio>(),
       baseUrl: get<String>(instanceName: 'baseUrl')));
-  gh.factory<_i5.ArticlesRepository>(
-      () => _i5.ArticlesRepository(get<_i4.ApiClient>()));
-  gh.factory<_i6.ArticlesBloc>(() => _i6.ArticlesBloc(get<_i7.Logger>(),
-      repository: get<_i5.ArticlesRepository>()));
-  gh.singleton<_i7.Logger>(loggerModule.logger());
+  gh.factory<_i6.ArticlesRepository>(
+      () => _i6.ArticlesRepository(get<_i5.ApiClient>()));
+  gh.factory<_i7.ArticlesBloc>(() => _i7.ArticlesBloc(get<_i4.Logger>(),
+      repository: get<_i6.ArticlesRepository>()));
   return get;
 }
 
--- lib/network/api_client.g.dart
+++ lib/network/api_client.g.dart
@@ -6,6 +6,8 @@ part of 'api_client.dart';
 // RetrofitGenerator
 // **************************************************************************
 
+// ignore_for_file: unnecessary_brace_in_string_interps
+
 class _ApiClient implements ApiClient {
   _ApiClient(this._dio, {this.baseUrl});
 
@@ -17,10 +19,11 @@ class _ApiClient implements ApiClient {
   Future<List<Article>> getArticles() async {
     const _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
     final _data = <String, dynamic>{};
     final _result = await _dio.fetch<List<dynamic>>(
         _setStreamType<List<Article>>(
-            Options(method: 'GET', headers: <String, dynamic>{}, extra: _extra)
+            Options(method: 'GET', headers: _headers, extra: _extra)
                 .compose(_dio.options, '/articles',
                     queryParameters: queryParameters, data: _data)
                 .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
